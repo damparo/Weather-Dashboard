012e5340f8a0a73b0d651195ffb1377c924567f7
Dennis Amparo <dennisamparo11@gmail.com>
2020-02-23 18:20:39 -0700

    arrangedcards

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
+// use city input to redo call when city is clickd in city field
 var justDoit = [];
 $("#search").on("click", function(event) {
     event.preventDefault();
-
-    
     var city = $("#city-input").val();
     var apiKey = "9f3310b38a0b78b3d966eb1da9d1599e";
-
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
 
     $.ajax({
     url: queryURL,
     method: "GET"
     })
-
     .then(function(response1){
         console.log(queryURL);
         console.log(response1);
     
-    // localStorage.setItem("citydata", JSON.stringify(city));
-    localStorage.setItem("citydata", JSON.stringify(justDoit));
-
-
     var temp = response1.main.temp;
     var humid = response1.main.humidity;
     var wind = response1.wind.speed;
     var uvlon = response1.coord.lon;
     var uvlat = response1.coord.lat;
     var image = response1.weather[0].icon;
-    var wIcon = "http://openweathermap.org/img/w/" + image + ".png";
+    var wIcon = "https://openweathermap.org/img/w/" + image + ".png";
     // var uv = $("#weatherdetails");
     console.log(uvlat);
 
-    $("#weatherdetails").append(
+    $("#weatherdetails1").append(
         $("<h3>").text(city),
         $("<p>").text("Temperature (F): " + temp),
         $("<p>").text("Humidity: " + humid + " %"),
@@ -40,7 +33,7 @@ $("#search").on("click", function(event) {
 
         // "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png"
 
-    $("#weatherdetails").empty()
+    $("#weatherdetails1").empty()
     );
 
     var uvqueryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + uvlat + "&lon=" + uvlon;
@@ -50,20 +43,13 @@ $("#search").on("click", function(event) {
     url: uvqueryURL,
     method: "GET"
     }).then(function(response2){
-        // console.log(response2);
-        // uv.text(response2.value);
         console.log(response2.value);
 
-        $("#weatherdetails").append(
+        $("#weatherdetails1").append(
             $("<p>").text("UV Index: " + response2.value),
         );
 
     });
-    // $("#weatherdetails").append(
-    //     $("<p>").text("UV Index: " + response2.value),
-    // );
-
-
     })
 
 
@@ -76,69 +62,67 @@ $("#search").on("click", function(event) {
         console.log(forequeryURL);
         console.log(response3);
 
-    $("#weatherdetails").append(
-    $("<h4>").text("5 Day Forecast"));
+    // $("#weatherdetails2").append(
+    // $("<h4>").text("5 Day Forecast"),
+    $("#weatherdetails2").empty()
+    // );
 
     for (var i =0; i <5; i++){
     var tempFore = response3.list[i].main.temp;
     var humFore = response3.list[i].main.humidity;
     var howOutside = response3.list[i].weather[0].icon;
-    var imgHowOutside = "http://openweathermap.org/img/w/" + howOutside + ".png";
+    var imgHowOutside = "https://openweathermap.org/img/w/" + howOutside + ".png";
     console.log(humFore);
 
-    $("#weatherdetails").append(
+    $("#weatherdetails2").append(
         // $("<h4>").text("5 Day Forecast"),
-        $("<div>").addClass("card-deck").append("<div>").addClass("card").append("<div>").addClass("card-body").append(
+        
+        $("<div>").addClass("card-deck").append("<div>").addClass("card col-2").append("<div>").addClass("card-body").append(
             $("<img>").attr("src", imgHowOutside),
             $("<p>").text("Temperature: " + tempFore),
             $("<br>"),
             $("<p>").text("Humidity: " + humFore)
-        ),
-    
-    )    
+           
+            ),
+    )
     };
-
+    
     });
     
     
+/* <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                  </div> */
 
-    function renderCities() {
-        // ("#cityfield").empty();
-        var location = $("<li>");
-        location.text(city);
-        // location.css("outline-color", "red");
-        $("#citieshere").append(location);
-        // var info = localStorage.getItem("citydata")
-        
-        // var displaycities = [];
-        
-        $("#citieshere").on("click", function(){
-        // // localStorage.setItem("citydata", JSON.stringify(city));
-        var places = JSON.parse(localStorage.getItem("citydata"));
-            justDoit = places;
-            justDoit.push(city);
-            $("#city-input").value = "";
-        
-        
-        // justDoit = places;
 
-    
-        
-        });
-
-    }
-
+    justDoit.push(city);  
+    localStorage.setItem("citydata", JSON.stringify(justDoit));
 
     renderCities();
 });
 
+function renderCities() {
+      
+    $("#citieshere").empty();
+    var places = JSON.parse(localStorage.getItem("citydata")) || [];
+    for ( var i = 0; i < places.length; i++) {
+        var location = $("<li>");
+        var cityButton = $("<button>");
+        cityButton.addClass("btn-block");
+        cityButton.text(places[i]);
+        location.append(cityButton);
+        $("#citieshere").append(location);
+    }; 
+
+    // //the onclick is on the entire field instead of the individual city - use event delegation
+}
 
 
+$("#citieshere").on("click", location, function(event){
+    event.preventDefault();
 
 
-
-
-
-
-
-
+});
